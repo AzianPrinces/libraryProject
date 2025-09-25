1821af6bd927d1bcdbe93438c50faed258bd28ec
Vladyslav Zavadskyi <vlazav01@easv365.dk>
2025-09-25 22:23:58 +0200

    Delete server/dataaccess/scaffold.sh

--- server/dataaccess/scaffold.sh
+++
@@ -1,8 +0,0 @@
-﻿dotnet ef dbcontext scaffold "Server=ep-late-bonus-ab1b9b6y-pooler.eu-west-2.aws.neon.tech;DB=neondb;UID=neondb_owner;PWD=npg_xjq3Zks8pSiO;SslMode=require" Npgsql.EntityFrameworkCore.PostgreSQL \
-  --project . \
-  --context LibraryDbContext \
-  --context-dir . \
-  --output-dir Entities \
-  --schema library \
-  --no-onconfiguring \
-  --force
