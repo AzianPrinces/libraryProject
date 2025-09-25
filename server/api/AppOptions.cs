3b2da71b89237c5b1a9cdff7305ac55d4f99357d
Vladyslav Zavadskyi <vlazav01@easv365.dk>
2025-09-25 22:38:05 +0200

    appOptions

---
+++ server/api/AppOptions.cs
@@ -0,0 +1,8 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace api;
+
+public sealed class AppOptions
+{
+    [Required] [MinLength(1)] public string ConnectionString { get; set; } = string.Empty;
+}
--- server/api/Program.cs
+++ server/api/Program.cs
@@ -1,6 +1,37 @@
+using api;
+using dataaccess;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddOptions<AppOptions>()
+    .Bind(builder.Configuration.GetSection("AppOptions"))
+    .ValidateDataAnnotations()
+    .ValidateOnStart();
+
+builder.Services.AddDbContext<LibraryDbContext>((sp, options) =>
+{
+    var appOptions = sp.GetRequiredService<IOptions<AppOptions>>().Value;
+    options.UseNpgsql(appOptions.ConnectionString);
+});
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+
 
 app.Run();
