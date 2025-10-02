3eadac59d8556e32eeb3eaf4cf915272bcca199d
Vladyslav Zavadskyi <vlazav01@easv365.dk>
2025-10-02 21:23:13 +0200

    test step 14

---
+++ server/tests/GenreTests.cs
@@ -0,0 +1,26 @@
+﻿using dataaccess;
+using dataaccess.Entities;
+
+namespace tests;
+
+public class GenreTests
+{
+    private readonly LibraryDbContext _context;
+    
+    public GenreTests(LibraryDbContext context)
+    {
+
+        _context = context;
+    }
+    
+    [Fact]
+    public void CanAddGenre()
+    {
+        var genre = new Genre { Id = Guid.NewGuid().ToString(), Name = "TestGenre" };
+        _context.Genres.Add(genre);
+        _context.SaveChanges();
+
+        var result = _context.Genres.FirstOrDefault(g => g.Name == "TestGenre");
+        Assert.NotNull(result);
+    }
+}
---
+++ server/tests/Startup.cs
@@ -0,0 +1,36 @@
+﻿using dataaccess;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Testcontainers.PostgreSql;
+using api;
+
+namespace tests;
+
+public class Startup
+{
+    private readonly PostgreSqlContainer _dbContainer;
+
+    public Startup()
+    {
+        _dbContainer = new PostgreSqlBuilder()
+            .WithDatabase("testdb")
+            .WithUsername("postgres")
+            .WithPassword("postgres")
+            .Build();
+        _dbContainer.StartAsync().Wait();
+    }
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddDbContext<LibraryDbContext>(options =>
+            options.UseNpgsql(_dbContainer.GetConnectionString()));
+
+        // Build a provider just for migration
+        var sp = services.BuildServiceProvider();
+        using var scope = sp.CreateScope();
+        var db = scope.ServiceProvider.GetRequiredService<LibraryDbContext>();
+
+        // Ensure database + schema exist
+        db.Database.EnsureCreated();
+    }
+}
